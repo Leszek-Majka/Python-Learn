3bd03a4bcb72013a83a99b58199337b758232b61
Leszek Majka <leszek.majka@cpk.pl>
2025-01-08 14:30:56 +0100

    Zmiana

--- Dictionaries.py
+++ Dictionaries.py
@@ -28,3 +28,8 @@ print(file_counts)
 file_counts = {"jpg":10, "txt":14, "csv":2, "py":23, 'cfg':8}
 del file_counts["cfg"]
 print(file_counts)
+
+
+file_counts = {"jpg":10, "txt":14, "csv":2, "py":23}
+for extension in file_counts:
+  print(extension)
